f948d08a0dc50fc57d61660661e629e62a38bb57
louis hoang <bawsing@gmail.com>
2014-09-14 17:13:30 -0400

    add uniqeness to task

--- app/models/task_list.rb
+++ app/models/task_list.rb
@@ -2,7 +2,7 @@ class TaskList < ActiveRecord::Base
   has_many :user_task_associations
   has_many :task_lists_tasks_associations
   has_many :users, through: :user_task_associations
-  has_many :tasks,  through: :task_lists_tasks_associations
+  has_many :tasks,  through: :task_lists_tasks_associations, dependent: :destroy
 
   validates :name, presence: true
 end

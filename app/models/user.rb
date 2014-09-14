5338a2e5ad310ccc708c091dbb6158fb4384e594
louis hoang <bawsing@gmail.com>
2014-09-14 17:08:35 -0400

    validate email

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ActiveRecord::Base
   has_many :task_lists, through: :user_task_associations
   has_many :user_task_associations
 
-  validates :email, presence: true
+  validates :email, presence: true, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
   validates :first_name, presence: true, if: "last_name.nil?"
 end
